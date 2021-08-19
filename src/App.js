f3f37f5e87ace798d4f3ba2af1541fc20b94a4d5
Andr-Shch <74563519+cybertrip-glitch@users.noreply.github.com>
2021-08-19 15:19:21 +0300

    pre pagin

--- src/App.js
+++ src/App.js
@@ -10,23 +10,27 @@ import { usePost } from "./hooks/usePost";
 import axios from "axios";
 import PostService from "./API/postService";
 import Loader from "./UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 
 function App() {
     const [posts, setPosts] = useState([  ])
-    ///////////////////////////////////////////////////////
     const [filter, setFilter] = useState({sort:'', query:''})
     const [modal, setModal]=useState(false)
-    const [isPostLanding, setIsPostLanding] = useState(false) 
+    const [totalCount, setTotalCount] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+    const [fetchPosts, isPostLanding, postError]= useFetching( async()=>{
+    
+      const respons = await PostService.getAll(limit, page)
+      setPosts(respons.data)
+      console.log(respons.headers['x-total-count']);
+      setTotalCount(respons.headers['x-total-count'])
+    })
 
     const sortedAndSearcgedPost = usePost(posts, filter.sort, filter.query)
     
-    async function fetchPosts(){
-      setIsPostLanding(true)
-      const posts = await PostService.getAll()
-      setPosts(posts)
-      setIsPostLanding(false)
-    }
+ 
     
     useEffect(()=>{
       fetchPosts()
@@ -60,7 +64,7 @@ function App() {
             
        <PostFilter filter={filter} setFilter={setFilter} />
      
-        
+       {postError&&<h1>Errorrrr.......</h1>} 
         {isPostLanding
           ? <Loader/>
           : <PostList remove={removePost} posts={sortedAndSearcgedPost} title='Post List'/>
